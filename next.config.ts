b9dda2a4d55e081b880ddc44263e45ca3a6f2919
Stan K <motrhelp@gmail.com>
2024-12-29 09:24:03 +0100

    Make /clouds a homepage

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
+// While there is no other functionality, /clouds is the home page.
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/clouds',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
